f96765b770b44621602ae9be182ec4466715f277
PietjeGeluk <62058850+PietjeGeluk@users.noreply.github.com>
2020-03-17 17:26:54 +0100

    Add files via upload
    Method call with parameters !=

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp39
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Voer een getal in onder de 10");
+            string guess = Convert.ToString(Console.ReadLine());
+            string CorrectNumber = "10";
+
+            if (guess != CorrectNumber)
+                PrintColorMessage(ConsoleColor.Red, "Verkeerd nummer, probeer opnieuw");
+            else
+                Console.WriteLine("GOED GEANTWOORD!");
+        }
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
